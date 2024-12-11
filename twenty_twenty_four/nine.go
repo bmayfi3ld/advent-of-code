a0e44074b0c1679b31a63a82f923e94cfcd3fa05
Brandon Mayfield <bmayfiel@cisco.com>
2024-12-11 17:28:58 -0600

    finished 9

--- twenty_twenty_four/nine.go
+++ twenty_twenty_four/nine.go
@@ -13,8 +13,8 @@ import (
 func init() {
 	cmd.RegisterCommand("2024-9-1-test", func() error { return nineOne(nineTestInput) })
 	cmd.RegisterCommand("2024-9-1-real", func() error { return nineOne(nineRealInput) })
-	cmd.RegisterCommand("2024-9-2-test", func() error { return nineOne(sixInput) })
-	cmd.RegisterCommand("2024-9-2-real", func() error { return nineOne(sixInput) })
+	cmd.RegisterCommand("2024-9-2-test", func() error { return nineTwo(nineTestInput) })
+	cmd.RegisterCommand("2024-9-2-real", func() error { return nineTwo(nineRealInput) })
 }
 
 func nineOne(input string) error {
@@ -54,7 +54,83 @@ func nineOne(input string) error {
 		}
 	}
 
-	fmt.Println(parsed)
+	// fmt.Println(parsed)
+
+	fmt.Println(calcSum(parsed))
+
+	return nil
+}
+
+func nineTwo(input string) error {
+	fmt.Println("hello")
+
+	defer wrapper.ProfileFunction("nineOne")()
+
+	parsed := expandDiskFormat(input)
+
+	for i := len(parsed) - 1; i > 0; i-- {
+		if parsed[i] == -1 {
+			continue
+		}
+
+		// check how big the file is
+		sizeOfFile := 0
+		fileNum := parsed[i]
+
+		for ii := i; ii > 0; ii-- {
+			if parsed[ii] != fileNum {
+				break
+			}
+
+			sizeOfFile++
+		}
+
+		// from the front, find the next spot that will fit
+		for ii := 0; ii < len(parsed); ii++ { //nolint: varnamelen
+			if parsed[ii] != -1 {
+				continue
+			}
+
+			// don't move the file to the right
+			// not past where it already is
+			if ii > i-sizeOfFile {
+				break
+			}
+
+			// see how big it is
+			sizeOfEmptySpot := 0
+
+			for iii := ii; iii < len(parsed); iii++ {
+				if parsed[iii] != -1 {
+					break
+				}
+
+				sizeOfEmptySpot++
+			}
+
+			// if it doesn't fit, skip the empty spot
+			if sizeOfFile > sizeOfEmptySpot {
+				ii += sizeOfEmptySpot
+
+				continue
+			}
+
+			// if fits swap it
+			for f := range sizeOfFile {
+				parsed[i-f] = -1
+				parsed[ii+f] = fileNum
+			}
+
+			// fmt.Println(parsed)
+
+			break
+		}
+
+		i -= (sizeOfFile - 1)
+
+	}
+
+	// fmt.Println(parsed)
 
 	fmt.Println(calcSum(parsed))
 
@@ -66,8 +142,9 @@ func calcSum(formatted []int) int {
 
 	for i, item := range formatted {
 		if item == -1 {
-			break
+			continue
 		}
+
 		sum += i * item
 	}
 
