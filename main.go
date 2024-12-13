01ad5ef1ba7e9029d507abf276321c61e0f789be
Brandon Mayfield <bmayfiel@cisco.com>
2024-12-12 19:43:00 -0600

    cleanup

--- main.go
+++ main.go
@@ -4,23 +4,35 @@ import (
 	"fmt"
 
 	"github.com/bmayfi3ld/advent-of-code/pkg/cmd"
+	"github.com/pkg/errors"
 
 	_ "github.com/bmayfi3ld/advent-of-code/twenty_twenty_four"
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Printf("Error: %+v", err)
+	}
+}
+
+func run() error {
+	// cpu profiling
 	// cpuFile, err := os.Create("cpu.prof")
 	// if err != nil {
-	// 	log.Fatal("could not create CPU profile: ", err)
+	// 	return errors.WithStack(err)
 	// }
 	// defer cpuFile.Close()
 
-	// if err := pprof.StartCPUProfile(cpuFile); err != nil {
-	// 	log.Fatal("could not start CPU profile: ", err)
+	// err = pprof.StartCPUProfile(cpuFile)
+	// if err != nil {
+	// 	return errors.WithStack(err)
 	// }
 	// defer pprof.StopCPUProfile()
 
-	if err := cmd.GetRootCmd().Execute(); err != nil {
-		fmt.Printf("Error: %+v", err)
+	err := cmd.GetRootCmd().Execute()
+	if err != nil {
+		return errors.WithStack(err)
 	}
+
+	return nil
 }
