da52fb4c28015e2f9e835592b15a74bf5ae76d9e
Brandon Mayfield <bmayfiel@cisco.com>
2024-12-12 19:47:17 -0600

    removed aoc text

--- twenty_twenty_four/eleven.go
+++ twenty_twenty_four/eleven.go
@@ -48,15 +48,6 @@ func eleven(input string, loopCount, answer int) error {
 }
 
 // apply rules from day 11
-// If the stone is engraved with the number 0, it is replaced by a stone
-// engraved with the number 1.
-// If the stone is engraved with a number that has an even number of digits,
-// it is replaced by two stones. The left half of the digits are engraved on
-// the new left stone, and the right half of the digits are engraved on the new
-// right stone. (The new numbers don't keep extra leading zeroes: 1000 would
-// become stones 10 and 0.)
-// If none of the other rules apply, the stone is replaced by a new stone; the
-// old stone's number multiplied by 2024 is engraved on the new stone.
 func applyRulesToSlice(currentMap map[int]int) map[int]int {
 	rule3Multiplier := 2024
 
